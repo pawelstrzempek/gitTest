8b23fe3eb5fc5c2d5e70872a2bd5a25e5b314d35
pawelstrzempek <pawel.strzempek@uj.edu.pl>
2016-11-07 16:56:33 +0100

    ff initialization

--- hdl/leder.vhd
+++ hdl/leder.vhd
@@ -41,8 +41,8 @@ CLK : in std_logic
 end leder;
 
 architecture Behavioral of leder is
-signal counter : std_logic_vector(31 downto 0) := (others=>'0');
-signal led_sgn : std_logic_vector(7 downto 0):= x"00";
+signal counter : std_logic_vector(30 downto 0) := (others=>'0');
+signal led_sgn : std_logic_vector(7 downto 0):= x"ff";
 signal dutycnt : std_logic_vector(7 downto 0):= x"00";
 signal duty_sgn : std_logic_vector(31 downto 0)  := (others=>'0');
 signal clk_sgn : std_logic := '0';
